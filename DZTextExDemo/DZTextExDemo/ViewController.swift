55f04695d8a9d3d2d37ee8c055e93b2a69a9d34c
songdezhong <songdezhong@58ganji.com>
2016-01-06 18:18:10 +0800

    初始化项目

---
+++ DZTextExDemo/DZTextExDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DZTextExDemo
+//
+//  Created by song_dzhong on 16/1/6.
+//  Copyright © 2016年 song_dzhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.lightGrayColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
